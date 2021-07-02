e552287dabb5d929202fb922e21a00f9a326405d
Adetola Bewaji <adetola.bewaji@fintechorg.co.uk>
2021-07-02 14:21:55 +0100

    db setup done and documented on README.md

--- READMe.md
+++ READMe.md
@@ -1,12 +1,40 @@
-To set flask to listen for changes do the following
+## To set flask to listen for changes do the following
+
 export FLASK_ENV=development
-export FLASK_APP=server.pygit push -u origin main
+export FLASK_APP=server.py
 
+## Set up version control
 
-##Set up version control
 $ git config --global user.name "Your Name"
 $ git config --global user.email "you@youraddress.com"
 $ git config --global push.default matching
 $ git config --global alias.co checkout
 $ git init
 
+## Install MySQL locally - use the following links to get started
+
+# Mac installation guide; https://flaviocopes.com/mysql-how-to-install/
+
+# Utuntu 20.4 installation guide; https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-20-04?fbclid=IwAR0Y3ocKmn8srWdlxb3IbutqDyuV2UGvh_NTCoFZS2vu-AhulcyCtx8myRU
+
+## Create MySQL db on using the following lines of code
+
+```
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password123",
+        auth_plugin='mysql_native_password'
+    )
+
+    my_cursor = mydb.cursor()
+
+    my_cursor.execute("CREATE DATABASE DebateForumDB")
+
+    my_cursor.execute("SHOW DATABASES")
+
+    for db in my_cursor:
+        print(db)
+```
--- server.py
+++ server.py
@@ -1,43 +1,92 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.fields.core import DateTimeField, IntegerField
 from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
+from flaskext.mysql import MySQL
+
 # Create a Flask instance
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password123'
+app.config['MYSQL_DB'] = 'DebateForumDB'
+
+# SECRET KEY
 app.config['SECRET_KEY'] = "my super secret"
 
+# Initialize the db
+db = MySQL(app)
+
+# Creating a connection cursor
+# cursor = db.connection.cursor()
+
 # Create a form class
-class NamerForm(FlaskForm):
-    name = StringField("What is your Name", validators=[DataRequired()])
+
+
+class TopicForm(FlaskForm):
+    topic = StringField("Add New Topic", validators=[DataRequired()])
+    author = IntegerField("Add New Topic", validators=[DataRequired()])
+    time = DateTimeField(validators=[DataRequired()])
     submit = SubmitField("Submit")
-#safe
-#capitalize
-#lower
-#upper
-#title
-#trim
+
+
+class TopicForm(FlaskForm):
+    topic = StringField("Add New Topic", validators=[DataRequired()])
+    name = StringField("Add New Topic", validators=[DataRequired()])
+    time = DateTimeField(validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# safe
+# capitalize
+# lower
+# upper
+# title
+# trim
 # striptags
 
 # Create a route decorator
+
+
 @app.route('/')
 def index():
     first_name = "john"
     stuff = "This is <strong>Bold</strong> text"
 
     favorite_pizza = ["Pepperani", "Cheese", "Orange", "Onions", 41]
-    return render_template("index.html", first_name=first_name, stuff = stuff, favorite_pizza=favorite_pizza)
+    return render_template("index.html", first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
 
 # Invalid URL
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
 # Internal Server Error
+
+
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
 
+# Create Topic Page
+
+
+@app.route('/topic', methods=['GET', 'POST'])
+def topic():
+    topic = None
+    form = TopicForm()
+    # validate Form
+    if form.validate_on_submit():
+        topic = form.topic.data
+        form.topic.data = ''
+        flash("Form Submitted Successfully")
+
+    return render_template("topic.html", topic=topic, form=form)
